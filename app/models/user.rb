72e2334d370844d94af6809d351b55f565803362
Your Name <you@example.com>
2023-01-06 00:16:13 +0100

    validate booking

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,10 @@
 class User < ApplicationRecord
   has_many :bookings
   has_many :cars, through: :bookings
+  validates :email, presence: true, uniqueness: true
+  validates :email, format: { with: URI::MailTo::EMAIL_REGEXP }
+  validates :username, presence: true, uniqueness: true
+  validates :password,
+            length: { minimum: 6 },
+            if: -> { new_record? || !password.nil? }
 end
