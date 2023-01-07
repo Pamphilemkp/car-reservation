0e10252e2153075247337b6d787cfb3061fbe261
Mark Otuya <markotuya0@gmail.com>
2023-01-07 09:51:14 +0100

    Update car.rb

--- app/models/car.rb
+++ app/models/car.rb
@@ -1,10 +1,4 @@
 class Car < ApplicationRecord
   has_many :bookings
   has_many :users, through: :bookings
-
-  validates :name, presence: true
-  validates :description, presence: true
-  validates :brand, presence: true
-  validates :daily_rate, presence: true, numericality: { greater_than: 0 }
-  validates :car_type, presence: true
 end
